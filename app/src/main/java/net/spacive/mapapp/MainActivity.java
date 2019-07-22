173dbfedc27ab57844c632f150f7816197e1bca6
Juraj Haluska <haluskajuraj0@gmail.com>
2019-07-22 16:14:49 +0200

    add permission handling logic

--- app/src/main/java/net/spacive/mapapp/MainActivity.java
+++ app/src/main/java/net/spacive/mapapp/MainActivity.java
@@ -1,16 +1,23 @@
 package net.spacive.mapapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_LOCATION_PERMISSION = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,40 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
         NavigationUI.setupWithNavController(navView, navController);
+
+        askForPermissions();
     }
 
+    private void askForPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                showPermissionRationale();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_LOCATION_PERMISSION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                showPermissionRationale();
+            }
+        }
+    }
+
+    private void showPermissionRationale() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.title_warning)
+                .setMessage(R.string.dialog_rationale)
+                .setPositiveButton(R.string.action_ok, null)
+                .setOnDismissListener(dialogInterface -> finish())
+                .create()
+                .show();
+    }
 }
